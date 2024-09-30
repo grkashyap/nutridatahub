2d49b44a8fbd24eb546c7396eb9ca7b247b9d50a
Rahul <grkashyap@gmail.com>
2024-09-30 12:22:57 +0530

    Updated to return response

--- nutritrack-datasync-deltaexports/src/DownloadDeltaFiles.py
+++ nutritrack-datasync-deltaexports/src/DownloadDeltaFiles.py
@@ -6,7 +6,8 @@ import requests
 from src.objectstorage.GetObjectStorage import get_object_storage
 
 def lambda_handler(event, context):
-    download_delta_files()
+    result = download_delta_files()
+    return result
 
 def download_delta_files():
     """
@@ -48,30 +49,21 @@ def download_delta_files():
             response.raise_for_status()
         except requests.exceptions.HTTPError as e:
             print(f'Error while reading file {file_name} from url {file_url} : {e}')
-            return_failure_response(e)
+            return return_failure_response(e)
 
         object_storage_connector.upload_object_to_bucket(file_name=file_name, file_content=response.content)
         actual_exported_files = actual_exported_files+1
 
-    return_success_response(num_files=actual_exported_files)
+    return return_success_response(num_files=actual_exported_files)
 
 
 def return_success_response(num_files):
-    result = {
-        "message": f'Successfully uploaded {num_files} to object storage',
-        "data": num_files
-    }
     return {
         "statusCode": 200,
         "body": f'Successfully uploaded {num_files} to object storage'
     }
 
 def return_failure_response(error):
-    result = {
-        "message": "Failed to upload files",
-        "data": error
-    }
-
     return {
         "statusCode": 500,
         "body": f'Failed to upload files due to error: {error}'
