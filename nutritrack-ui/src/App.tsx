3a525f5efcf68ef672ef174b8978f51656f51290
Rahul <grkashyap@gmail.com>
2024-11-14 00:19:46 +0530

    Added tailwind implementation

--- nutritrack-ui/src/App.tsx
+++ nutritrack-ui/src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import SearchField from './components/SearchField';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <SearchField />
+    <div className="flex-grow md:container md:mx-auto shadow-xl min-h-full">
+      <Header />
+      <div className="grid md:grid-flow-col grid-cols-4 grid-row gap-4">
+        <SearchField />  
+      </div>
     </div>
   );
 }
--- nutritrack-ui/src/components/SearchField.tsx
+++ nutritrack-ui/src/components/SearchField.tsx
@@ -1,11 +1,15 @@
-import './SearchField.css';
-
 function SearchField() {
 
     return (
-        <div className="search-form">
+        <div className="col-span-4">
             <form action='#'>
-                <input className="search-field" placeholder="Search for Products" />
+                <input 
+                    className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 
+                        rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 
+                        focus:ring-sky-500 focus:ring-1 sm:text-sm" 
+                    placeholder="Search for products..." 
+                    type="text" 
+                    name="search"/>    
             </form>
         </div>
     )
